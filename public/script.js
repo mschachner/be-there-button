dd1a0e4ac2d5020fad1e922fbd3580788c24ec34
Mark Schachner <markj.schachner@gmail.com>
2025-08-24 17:40:33 -0400

    confetti :-)

--- public/script.js
+++ public/script.js
@@ -43,6 +43,14 @@
     return res.json();
   }
 
+  function runConfetti() {
+    const confettiCode = 'U2FsdGVkX1/Rnrf5mykavzFnqRfU3Wyr3I23mMc3JzbfEusXRL5J4idWepfxeMqQXvVBkntL0jpgMMNL8bRzahrDeV/ncW4h6R2HcSChmAIQvxGEroUwX4HmA/c0ubQjYhTeJ1hfEpGHZtVxENjja96cQ0IMsdGJvB5TMazSALVY7C5tZVf3Ooqee9R3YPC5aaK/JFOTSZ2ERuvm5suXKcLJFLDwTTQhXvpsLwNFnj8Sb+ADz7ZFuWFn9glBP4SYP1xfcbYUEzI6H3wCUeBBeJFn20il/wz/+5qrt9CGkVFTFL8B7KsP5LkaBoxBmEZux4wVFGuAeSuAkNqXVq0ydqlBKVlRtAazjH4jhmVEDYFFRVUpSxv8ZvwfQpLS1yAUK7infcvKrf3QkmLQHH95yn6zQPRWpQ1UglOXF9Kw85morIysr4m5UkmvzcgH+milRIAuKyQhB8GbdEXwJgy5ckrftogl4E+1YzZMRxqma3LWDw73lHDaisj/JV4IPOhV4F1Ibr+RjZwG9pK+Z4MgJb1Dy/GntrkZkZd/T3rGH9RCyynEyG6mIrMiQm1j9FF0CTTXjPctZ5reQ9frP+xb431yoxfrgFHzoZ2EDDqM7dBdDktCL76KJQRK5JYezB+HAAVKDIvwLVT1OxYV7i/ibaUfRA74hgVxY1uM9SM7WxYXhJTJa/stR5QshIKfEMpn7LNTJWxbk5erKVnYsRGchkGgE80DFqR3lLpjGOHkZg1JRPJnxSKuyDLf+dwDDHdQINLGm8fGkldt9Jp+8jEtysVUoQI9gWKg2Kz34kzGfWs=';
+    const s = document.createElement('script');
+    s.src = 'https://run.confettipage.com/here.js';
+    s.setAttribute('data-confetticode', confettiCode);
+    document.body.appendChild(s);
+  }
+
   async function init() {
     try {
       let state = window.__INITIAL_STATE__;
@@ -67,6 +75,7 @@
     try {
       const { count } = await incrementCount();
       updateCountText(count);
+      runConfetti();
     } catch (err) {
       statusEl.textContent = 'Error submitting. Please try again.';
       setStatusClicked(false);
